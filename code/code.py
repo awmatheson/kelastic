2ec49c19dd7a0918b6dac55cf769c46359dabef8
awmatheson <awmatheson@github.com>
2020-03-29 13:32:54 -0400

    test output

---
+++ code/code.py
@@ -0,0 +1,29 @@
+def convert_to_markdown(metrics_dict):
+    markdown = ""
+    for k in metrics_dict.keys():
+        markdown += f"## {k} \\n\\n"
+
+        # format headers
+        headers = "|"
+        for nam in metrics_dict[k].keys():
+            headers += f" {nam} |"
+        markdown += headers + "\\n"
+
+        # add lines under headers
+        markdown += "|" + " -- |" * len(metrics_dict[k]) + "\\n"
+
+        # add values
+        metrics = "|"
+        for val in metrics_dict[k].values():
+            try:
+                val = float(val)
+                metrics += f" {val:.3} |"
+            except ValueError:
+                metrics += f" {val} |"
+        markdown += metrics + "\\n"
+
+    return markdown
+
+run_metrics = {'GitHubActionExperiment_1585169537_0149ea05': {'Kernel type': 'linear', 'Penalty': 1.0, 'Accuracy': 0.9333333333333333, 'precision': 0.9333333333333333, 'recall': 0.9333333333333333, 'f1-score': 0.9333333333333333, 'confusion_matrix': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix', 'confusion_matrix_unnormalized': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix_unnormalized_1585170650.png', 'confusion_matrix_normalized': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix_normalized_1585170651.png', 'Model Name': 'model.pkl'}}
+metrics_markdown = convert_to_markdown(run_metrics)
+print(f"::set-output name=run_metrics::{metrics_markdown}")
