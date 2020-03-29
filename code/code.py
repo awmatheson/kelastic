eabcf9d990da7df3056109303268218c62c74d67
awmatheson <awmatheson@github.com>
2020-03-29 13:41:55 -0400

    python error

--- code/code.py
+++ code/code.py
@@ -1,3 +1,5 @@
+# python script
+
 def convert_to_markdown(metrics_dict):
     markdown = ""
     for k in metrics_dict.keys():
@@ -24,6 +26,7 @@ def convert_to_markdown(metrics_dict):
 
     return markdown
 
-run_metrics = {'GitHubActionExperiment_1585169537_0149ea05': {'Kernel type': 'linear', 'Penalty': 1.0, 'Accuracy': 0.9333333333333333, 'precision': 0.9333333333333333, 'recall': 0.9333333333333333, 'f1-score': 0.9333333333333333, 'confusion_matrix': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix', 'confusion_matrix_unnormalized': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix_unnormalized_1585170650.png', 'confusion_matrix_normalized': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix_normalized_1585170651.png', 'Model Name': 'model.pkl'}}
-metrics_markdown = convert_to_markdown(run_metrics)
-print(f"::set-output name=run_metrics::{metrics_markdown}")
+if __name__ == "__main__":
+    run_metrics = {'GitHubActionExperiment_1585169537_0149ea05': {'Kernel type': 'linear', 'Penalty': 1.0, 'Accuracy': 0.9333333333333333, 'precision': 0.9333333333333333, 'recall': 0.9333333333333333, 'f1-score': 0.9333333333333333, 'confusion_matrix': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix', 'confusion_matrix_unnormalized': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix_unnormalized_1585170650.png', 'confusion_matrix_normalized': 'aml://artifactId/ExperimentRun/dcid.GitHubActionExperiment_1585169537_0149ea05/confusion_matrix_normalized_1585170651.png', 'Model Name': 'model.pkl'}}
+    metrics_markdown = convert_to_markdown(run_metrics)
+    print(f"::set-output name=run_metrics::{metrics_markdown}")
